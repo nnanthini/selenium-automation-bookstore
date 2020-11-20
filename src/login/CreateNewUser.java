10d11d8c33050d6d17d37249eb4b1295d14e693d
nnanthini <nanthinin92@gmail.com>
2020-11-20 15:47:40 -0600

    Add new files

---
+++ src/login/CreateNewUser.java
@@ -0,0 +1,12 @@
+package login;
+
+import org.testng.annotations.Test;
+
+public class CreateNewUser {
+  @Test
+  public void f() {
+
+	
+	  
+  }
+}
---
+++ src/login/LoginUser.java
@@ -0,0 +1,9 @@
+package login;
+
+import org.testng.annotations.Test;
+
+public class LoginUser {
+  @Test
+  public void f() {
+  }
+}
