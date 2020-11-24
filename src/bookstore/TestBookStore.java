0b2ead3c09725a1c8fb4236e3fca9ee3fcafcf88
nnanthini <nanthinin92@gmail.com>
2020-11-23 19:28:18 -0600

    Update select dropdown

--- src/bookstore/TestBookStore.java
+++ src/bookstore/TestBookStore.java
@@ -3,9 +3,13 @@ package bookstore;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -39,14 +43,39 @@ public class TestBookStore {
   }
   
   @Test (dependsOnMethods = "initialisePage")
-  public void testBottomSpan() {
+  public void testSelectDropDown() {
 	  int actualCount, expectedCount;
 	  Select select = new Select(driver.findElement(By.xpath("//select[@aria-label='rows per page']")));
-	  
+	  JavascriptExecutor js = (JavascriptExecutor) driver;
+	  js.executeScript("arguments[0].scrollIntoView()", select);
 	  Assert.assertFalse(select.isMultiple(), "Rows per page is single select");
 	  expectedCount = 6;
 	  actualCount = select.getOptions().size();
 	  Assert.assertEquals(actualCount, expectedCount, "Mismatch in number of options in select");
+	  
+	  int count = 0;
+	  while (count < 6) {
+		  select.selectByIndex(count);
+		  ValidatePageInput();
+		  ++count;
+	  }
+  }
+  
+  public void ValidatePageInput() {
+	  WebElement pgNoInput = driver.findElement(By.xpath("//input[@aria-label='jump to page']"));
+	  String totalPages = driver.findElement(By.xpath("//span[@class='-totalPages']")).getText();
+	  Actions actions = new Actions(driver);
+	  int i = 1;
+	  while (i <= Integer.parseInt(totalPages)) {
+		  actions.sendKeys(pgNoInput, Integer.toString(i)).sendKeys(Keys.ENTER).build().perform();
+		  i++;
+		  //Assert.assertTrue(i<=Integer.parseInt(totalPages), "");
+	  }
+	  actions.sendKeys(pgNoInput, Integer.toString(i)).sendKeys(Keys.ENTER).perform();
+	  //String o = pgNoInput.getText();
+	  String finalInputPgNo = pgNoInput.getAttribute("value");
+	  Assert.assertFalse(finalInputPgNo.equals(i), "Higher cap should not be able to input");
+	  Assert.assertEquals(finalInputPgNo, totalPages, "Input page is more than total number of pages");
 	  
   }
   
